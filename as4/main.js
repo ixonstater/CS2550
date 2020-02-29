d6bc13edfb58bd25beac9233dd394011be07b97b
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-02-29 11:08:37 -0700

    Added animation

--- as4/main.js
+++ as4/main.js
@@ -1,6 +1,11 @@
+function animateOnLoad(){
+
+}
+
 function main(){
     var instance = new GameInstance()
     instance.initGame()
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('load', animateOnLoad)
